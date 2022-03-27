0c2c24935277076340b7a6e15665ee155a6658ea
unknown <ashtalakshmi.xworkz@gmail.com>
2022-03-27 14:54:00 +0530

    test

---
+++ commands.sql
@@ -0,0 +1,12 @@
+create database xworkz;//create data base name//its like home first u have eto create
+
+create table xworkz.student1(
+id int(20),
+name varchar(50),
+rollno int(20),
+collegeName varchar(50));
+
+//insert into databasename.tablename values(value1,value2,value3...);//
+insert into xworkz.student values(1,'ashu',
+123,'AMC');
+select *from xworkz.student;
