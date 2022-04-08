c431691d65bc1a33dc27cb081ce02117958d06e6
Ashtalakshmi8 <ashtalakshmi.xworkz@gmail.com>
2022-04-08 16:27:44 +0530

    mobile

---
+++ mobile/MobileTester.java
@@ -0,0 +1,25 @@
+package com.xworkz;
+
+import com.xworkz.mobile.MobileDAOImpl;
+
+public class MobileTester {
+	
+		public static void main(String[] args) {
+			MobileDAOImpl mobileDAOImpl = new MobileDAOImpl();
+			 mobileDAOImpl.getMobileData();
+			 mobileDAOImpl.getAllMobileData();
+			 mobileDAOImpl.getMobilePriceByID();
+			// mobileDAOImpl.getMobileModelById();
+			// mobileDAOImpl.getMobilMobileColorById();
+			// mobileDAOImpl.getMobileRamById();
+			// mobileDAOImpl.getMobileTypeById();
+
+			// mobileDAOImpl.getMaxMobilePrice();
+			// mobileDAOImpl.getMinMobilePrice();
+			// mobileDAOImpl.getMobileCount();
+			// mobileDAOImpl.getMobileColorByBrand();
+			// mobileDAOImpl.getMobilePriceByBrand();
+			// mobileDAOImpl.getMobileTypeByPrice();
+			// mobileDAOImpl.getMobileModelByColor();
+		}
+	}
